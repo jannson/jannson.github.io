ba2ef05e78267a5360ce241902dba7e2ebe13420
gan <wei_shenggan@szhnd.com>
2014-02-12 17:45:04 +0800

    home page

--- meta.js
+++ meta.js
@@ -3,12 +3,12 @@
     "copyright": "jannson",
     "cates": [
         {"name": "tech", "text":"技术"},
-        {"name": "life", "text":"生活"},
+        {"name": "act", "text":"活动"},
         {"name": "other", "text":"其它"}
     ],
     "articles":[
         {"title": "tech hello world", "file": "2012-01-23", "cate":"tech"},
-        {"title": "art hello world", "file": "2012-02-23", "cate":"art"},
+        {"title": "art hello world", "file": "2012-02-23", "cate":"act"},
         {"title": "other hello world", "file": "2012-03-23", "cate":"other"}
         ]
 }
