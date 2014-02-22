8861999bef0eccf36d3f56983b24adc1a27bac8c
gan <wei_shenggan@szhnd.com>
2014-02-22 15:12:43 +0800

    update

--- gen.py
+++ gen.py
@@ -2,7 +2,7 @@ import markdown2
 import os, sys, codecs
 
 output = ''
-with codecs.open('post/index.txt', 'r', 'utf-8') as file:
+with codecs.open('index.txt', 'r', 'utf-8') as file:
     output = markdown2.markdown(file.read())
 with codecs.open('index.o', 'w', 'utf-8') as file:
     file.write(output)
