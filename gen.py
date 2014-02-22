a064b8e5e55d96211fabfe6923597df4a1b03c4a
gan <wei_shenggan@szhnd.com>
2014-02-22 14:58:46 +0800

    only home page

---
+++ gen.py
@@ -0,0 +1,10 @@
+import markdown2
+import os, sys, codecs
+
+output = ''
+with codecs.open('post/index.txt', 'r', 'utf-8') as file:
+    output = markdown2.markdown(file.read())
+with codecs.open('index.o', 'w', 'utf-8') as file:
+    file.write(output)
+
+
