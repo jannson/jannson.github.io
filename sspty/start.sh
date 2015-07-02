4f0a475f40b893fd09b24ef646ae09b05937b014
janson <gandancing@gmail.com>
2015-07-02 19:41:54 +0800

    change to http

--- sspty/start.sh
+++ sspty/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-srun=`ps -ef|grep sspty|grep -v grep`
-if [ "$srun" == "" ];then
+srun=`pgrep sspty`
+if [ "$srun" = "" ]; then
+    echo "sspty starting"
     wget -q http://jannson.github.io/sspty/sspty -O ./sspty
     chmod 755 ./sspty
-    pkill sspty
     nohup ./sspty > pty.log 2>&1 &
 fi
-
