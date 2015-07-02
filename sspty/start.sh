ed96cb3d5628b40ddf51d3adbc159db05cf0e629
janson <gandancing@gmail.com>
2015-07-02 18:10:18 +0800

    change to http

--- sspty/start.sh
+++ sspty/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-rm -rf ./sspty
-wget http://jannson.github.io/sspty/sspty
+wget -q http://jannson.github.io/sspty/sspty -O ./sspty
 chmod 755 ./sspty
 pkill sspty
 nohup ./sspty > pty.log 2>&1 &
